94de1c7166fc1427eef00e889d12e772c1e9f8ff
0xym0 <oxymoster@gmail.com>
2022-09-29 03:05:35 +0300

    Default generated test added.

---
+++ src/test/kotlin/ru/oxymo/dockertestui/DockerTestUiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.oxymo.dockertestui
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DockerTestUiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
