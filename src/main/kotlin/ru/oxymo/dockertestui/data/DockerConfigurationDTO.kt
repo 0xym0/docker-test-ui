2a6acb3fa9c38f1d7e83da6eef73ecb70925a31e
0xym0 <oxymoster@gmail.com>
2022-09-30 00:34:21 +0300

    Dialog form implemented for configuring docker connection properties. DockerConfigurationDTO class made mutable to use it as a form binding.

--- src/main/kotlin/ru/oxymo/dockertestui/data/DockerConfigurationDTO.kt
+++ src/main/kotlin/ru/oxymo/dockertestui/data/DockerConfigurationDTO.kt
@@ -1,8 +1,13 @@
 package ru.oxymo.dockertestui.data
 
+import ru.oxymo.dockertestui.util.Constants.EMPTY_STRING
+import javax.validation.constraints.NotEmpty
+
 data class DockerConfigurationDTO(
-    val dockerHost: String,
-    val dockerCertPath: String,
-    val dockerTlsVerify: Boolean,
-    val dockerApiVersion: String,
+    @NotEmpty
+    var dockerHost: String = EMPTY_STRING,
+    var dockerCertPath: String = EMPTY_STRING,
+    var dockerTlsVerify: Boolean = false,
+    @NotEmpty
+    var dockerApiVersion: String = EMPTY_STRING
 )
--- src/main/kotlin/ru/oxymo/dockertestui/ui/ConnectionDialog.kt
+++ src/main/kotlin/ru/oxymo/dockertestui/ui/ConnectionDialog.kt
@@ -3,14 +3,19 @@ package ru.oxymo.dockertestui.ui
 import com.vaadin.flow.component.Key
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.button.ButtonVariant
+import com.vaadin.flow.component.checkbox.Checkbox
 import com.vaadin.flow.component.dialog.Dialog
 import com.vaadin.flow.component.formlayout.FormLayout
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.binder.BeanValidationBinder
+import com.vaadin.flow.data.binder.ValidationException
 import com.vaadin.flow.spring.annotation.SpringComponent
 import com.vaadin.flow.spring.annotation.UIScope
 import org.springframework.beans.factory.annotation.Autowired
+import ru.oxymo.dockertestui.data.DockerConfigurationDTO
 import ru.oxymo.dockertestui.service.DockerConnector
+import ru.oxymo.dockertestui.util.CommonUtil
 
 @SpringComponent
 @UIScope
@@ -21,8 +26,18 @@ class ConnectionDialog @Autowired constructor(
     private final val dialog = Dialog()
     private final val dockerHostField = TextField("Docker host")
     private final val dockerAPIVersionField = TextField("Docker API version")
+    private final val dockerCertificatesPathField = TextField("Docker Cert local path")
+    private final val dockerTlsVerifyCheckbox = Checkbox("Is TLS enabled") {
+        dockerCertificatesPathField.isVisible = it.value
+    }
+    private final val formBinder = BeanValidationBinder(DockerConfigurationDTO::class.java)
 
     init {
+        formBinder.bind(dockerHostField, "dockerHost")
+        formBinder.bind(dockerAPIVersionField, "dockerApiVersion")
+        formBinder.bind(dockerTlsVerifyCheckbox, "dockerTlsVerify")
+        formBinder.bind(dockerCertificatesPathField, "dockerCertPath")
+
         dialog.headerTitle = "Docker settings"
         dialog.add(createDialogLayout())
         dialog.footer.add(createCancelButton())
@@ -35,6 +50,8 @@ class ConnectionDialog @Autowired constructor(
         val layout = FormLayout(
             dockerHostField,
             dockerAPIVersionField,
+            dockerTlsVerifyCheckbox,
+            dockerCertificatesPathField
         )
         layout.addClassName("docker-test-ui-configuration-form-layout")
         return layout
@@ -48,7 +65,19 @@ class ConnectionDialog @Autowired constructor(
         val button = Button("Save")
         button.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
         button.addClickListener {
-//            dockerConnector.updateDockerConfiguration()
+            formBinder.validate()
+            val configurationDTO = DockerConfigurationDTO()
+            try {
+                formBinder.writeBean(configurationDTO)
+            } catch (e: ValidationException) {
+                NotificationCreator.showErrorNotification(CommonUtil.getErrorTextFromException(e))
+                return@addClickListener
+            }
+            if (dockerConnector.updateDockerConfiguration(configurationDTO)) {
+                NotificationCreator.showSuccessNotification("New configuration saved successfully")
+            } else {
+                NotificationCreator.showErrorNotification("Error on applying new configuration")
+            }
             dialog.close()
         }
         button.addClickShortcut(Key.ENTER)
@@ -57,8 +86,7 @@ class ConnectionDialog @Autowired constructor(
 
     fun openDialog() {
         val configurationDTO = dockerConnector.getDockerConfigurationDTO()
-        dockerHostField.value = configurationDTO.dockerHost
-        dockerAPIVersionField.value = configurationDTO.dockerApiVersion
+        formBinder.readBean(configurationDTO)
         dialog.open()
     }
 
